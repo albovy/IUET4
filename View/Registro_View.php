4947c4e412e672508c3cef5cd44984e7fa6fb789
jqbergantinho <jqbergantinho@esei.uvigo.es>
2018-12-20 17:55:42 +0100

    Registro_View v1.0

--- View/Registro_View.php
+++ View/Registro_View.php
@@ -8,7 +8,35 @@
 
         function render(){
 ?>
-        <form action="" method='POST'></form>
+        <form action='../Controller/Registro_Controller.php' method='POST'>
+        
+            <label for="email"><?= $strings['Email'] ?></label>
+            <input type="email" id="email" name="email">
+            <label for="dni"><?= $strings['DNI'] ?></label>
+            <input type="text" id="DNI" name="DNI">
+            <label for="direccion"><?= $strings['Dirección'] ?></label>
+            <input type="text" id="direccion" name="direccion">
+            <label for="nombre"><?= $strings['Nombre'] ?></label>
+            <input type="text" id="nombre" name="nombre">
+            <label for="apellidos"><?= $strings['Apellidos'] ?></label>
+            <input type="text" id="apellidos" name="apellidos">
+            <label for="avatar"><?= $strings['Avatar'] ?></label>
+            <input type="file" id="avatar" name="avatar">
+            <label for="login"><?= $strings['Login'] ?></label>
+            <input type="text" id="login" name="login">
+            <label for="contraseña"><?= $strings['Contraseña'] ?></label>
+            <input type="password" id="contraseña" name="contraseña">
+            <label for="rol"><?= $strings['Rol'] ?></label>
+            <input type="text" name="rol">
+            <datalist id="rol">
+                <option value="Administrador">
+                <option value="Subastador">
+                <option value="Pujador">
+            </datalist>
+        
+        <a href="../index.php"></a>
+        
+        </form>
 
 
 
