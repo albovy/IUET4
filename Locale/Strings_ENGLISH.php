3994d124646c63f5e6fdb5924a61d77ebad264fd
jqbergantinho <jqbergantinho@esei.uvigo.es>
2018-12-20 18:14:16 +0100

    Cambios a los ficheros Strings

---
+++ Locale/Strings_ENGLISH.php
@@ -0,0 +1,22 @@
+<?php
+
+//Cambio de idioma
+
+$strings =
+array(
+    'Email' => 'Email',
+    'DNI' => 'DNI',
+    'Dirección' => 'Address',
+    'Nombre' => 'Name',
+    'Apellidos' => 'Surname',
+    'Avatar' => 'Avatar',
+    'Login' => 'Login',
+    'Contraseña' => 'Password',
+    'Rol' => 'Role',
+    'Administrador' => 'Administrator',
+    'Subastador' => 'Auctioneer',
+    'Pujador' => 'Bidder'
+
+)
+;
+?>
---
+++ Locale/Strings_GALLAECIAN.php
@@ -0,0 +1,22 @@
+<?php
+
+//Cambio de idioma
+
+$strings =
+array(
+    'Email' => 'Email',
+    'DNI' => 'DNI',
+    'Dirección' => 'Dirección',
+    'Nombre' => 'Nome',
+    'Apellidos' => 'Apelidos',
+    'Avatar' => 'Avatar',
+    'Login' => 'Login',
+    'Contraseña' => 'Contraseña',
+    'Rol' => 'Rol',
+    'Administrador' => 'Administrador',
+    'Subastador' => 'Subastador',
+    'Pujador' => 'Licitante'
+
+)
+;
+?>
